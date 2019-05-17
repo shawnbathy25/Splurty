7b2f299ea0a7c239c0861f1317e529437e1cefd2
Oluwaseun Oyogho <shawnbathy25@example.com>
2019-05-17 04:24:10 +0000

    code changes

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -3,8 +3,20 @@ class QuotesController < ApplicationController
     @quote = Quote.order("RANDOM()").first
   end
 
-  
+
    def new
      @quote = Quote.new
   end
-end
+
+
+  def create
+      Quote.create(quote_params)
+      redirect_to root_path
+  end
+
+  private
+
+  def quote_params
+    params.require(:quote).permit(:saying, :author)
+  end
+end 
